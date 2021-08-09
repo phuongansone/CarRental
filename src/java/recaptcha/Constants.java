6565315779aab15d75b920bec530057e092353b6
phuongansone <phuongansone@gmail.com>
2021-08-09 22:44:51 +0700

    Update Constants.java

--- src/java/recaptcha/Constants.java
+++ src/java/recaptcha/Constants.java
@@ -5,7 +5,7 @@ package recaptcha;
  * @author PC
  */
 public class Constants {
-    public static final String SITE_KEY = "6LcAxbkaAAAAAAKfYGNgDiNmBF5winyeQctGT2MA";
+    public static final String SITE_KEY = "";
 
-    public static final String SECRET_KEY = "6LcAxbkaAAAAAEALzyBBDkQQZbYZWOz6Cn78QGtF";
+    public static final String SECRET_KEY = "";
 }
